31ca104440f130a4a1d9fecb14d5c0047d96e81f
hugor <hugorezendemartins1256@gmail.com>
2025-04-09 18:17:12 -0300

    criação do repositorios e endpoints

--- Program.cs
+++ Program.cs
@@ -1,17 +1,35 @@
 using Microsoft.EntityFrameworkCore;
 using RepositoryStore.Data;
+using RepositoryStore.Models;
+using RepositoryStore.Repositories;
+using RepositoryStore.Repositories.Abstractions;
 
 var builder = WebApplication.CreateBuilder(args);
 var conectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
-builder.Services.AddDbContext<AppDbContext>(s =>
-{
-    s.UseSqlServer(conectionString);
+builder.Services.AddDbContext<AppDbContext>(s => { s.UseSqlServer(conectionString); });
 
-});
+builder.Services.AddTransient<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/v1/products", async (CancellationToken token, IProductRepository repository)
+    => Results.Ok(await repository.GetAllAsync()));
+
+app.MapGet("/v1/products/{id}", async (int id, CancellationToken token, IProductRepository repository)
+    => Results.Ok(await repository.GetByIdAsync(id)));
+
+app.MapPost("/v1/products", async (CancellationToken token, IProductRepository repository, Product product)
+    => Results.Ok(await repository.CreateAsync(product, token)));
+
+app.MapPut("/v1/products", async (CancellationToken token, IProductRepository repository, Product product)
+    => Results.Ok(await repository.UpdateAsync(product, token)));
+
+app.MapDelete("/v1/products/{id}", async (CancellationToken token, IProductRepository repository, int id)
+    =>
+{
+    var product = await repository.GetByIdAsync(id);
+    return product == null ? Results.NotFound() : Results.Ok(await repository.DeleteAsync(product, token));
+});
 
 app.Run();
---
+++ Repositories/ProductRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RepositoryStore.Data;
+using RepositoryStore.Models;
+using RepositoryStore.Repositories.Abstractions;
+
+namespace RepositoryStore.Repositories;
+
+public class ProductRepository(AppDbContext context) : IProductRepository
+{
+    public async Task<Product> CreateAsync(Product product, CancellationToken cancellationToken = default)
+    {
+        await context.Products.AddAsync(product, cancellationToken);
+        await context.SaveChangesAsync(cancellationToken);
+
+        return product;
+    }
+
+    public async Task<Product> UpdateAsync(Product product, CancellationToken cancellationToken = default)
+    {
+        context.Products.Update(product);
+        await  context.SaveChangesAsync(cancellationToken);
+        
+        return product;
+    }
+    
+    public async Task<Product> DeleteAsync(Product product, CancellationToken cancellationToken = default)
+    {
+        context.Products.Remove(product);
+        await  context.SaveChangesAsync(cancellationToken);
+        
+        return product;
+    }
+
+    public async Task<Product?> GetByIdAsync(int id, CancellationToken cancellationToken = default)
+    {
+        return await context.Products
+            .AsNoTracking()
+            .FirstOrDefaultAsync(s => s.Id == id, cancellationToken);
+    }
+    
+    public async Task<List<Product>?> GetAllAsync(CancellationToken cancellationToken = default)
+    {
+        return await context
+            .Products
+            .AsNoTracking()
+            .ToListAsync(cancellationToken);
+    }
+}
